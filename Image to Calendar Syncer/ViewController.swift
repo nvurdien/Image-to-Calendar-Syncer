51e96fbcccad5ac85131b9c5e796b454f5bbdc46
Navie Vurdien <nvurdien@gmail.com>
2018-02-03 13:29:18 -0800

    Initial commit

---
+++ Image to Calendar Syncer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Image to Calendar Syncer
+//
+//  Created by Navie Vurdien on 2/3/18.
+//  Copyright © 2018 Vurdien-Bucher-Software. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
---
+++ Image to Calendar SyncerTests/Image_to_Calendar_SyncerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Image_to_Calendar_SyncerTests.swift
+//  Image to Calendar SyncerTests
+//
+//  Created by Navie Vurdien on 2/3/18.
+//  Copyright © 2018 Vurdien-Bucher-Software. All rights reserved.
+//
+
+import XCTest
+@testable import Image_to_Calendar_Syncer
+
+class Image_to_Calendar_SyncerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
