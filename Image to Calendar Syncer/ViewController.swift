f7ff173bd1b8d8fcc52bdee97d2b6751c10fd70b
Charles Bucher <charles.abucher@gmail.com>
2018-02-03 17:42:40 -0800

    Added more control flow logic

--- Image to Calendar Syncer/ViewController.swift
+++ Image to Calendar Syncer/ViewController.swift
@@ -15,9 +15,16 @@ import UIKit
 //  Copyright © 2018 Vurdien-Bucher-Software. All rights reserved.
 //
 
+struct Event {
+    var location: String
+    var title: String
+}
+
 import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var capturedImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,25 +33,88 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let imagePicker = UIImagePickerController()
+        // When view appears, skip loading the image picker if there is a saved image
+        if capturedImage.image == nil {
         
-        // If the device has a camera, take a picture, otherwise,
-        // just pick from photo library
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            imagePicker.sourceType = .camera
+            let imagePicker = UIImagePickerController()
+            
+            // If the device has a camera, take a picture, otherwise,
+            // just pick from photo library
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePicker.sourceType = .camera
+            } else {
+                imagePicker.sourceType = .photoLibrary
+            }
+            
+            imagePicker.delegate = self
+            
+            // Place image picker on the screen
+            present(imagePicker, animated: true, completion: nil)
+            print("Present 1")
         } else {
-            imagePicker.sourceType = .photoLibrary
+            
+            if let result = processImage(image: capturedImage.image) {
+            
+                for event in result {
+                    
+                    // Present a editable view
+                    
+                }
+            } else {
+                
+                let badImageAlert = UIAlertController(title: "Unable to parse an event from the photo.",
+                                                      message: "Please try again.",
+                                                      preferredStyle: .alert)
+                
+                badImageAlert.addAction(UIAlertAction(title: "Ok", style: .default,
+                                                      handler:
+                    { (action: UIAlertAction!) in
+                        self.capturedImage.image = nil
+                        
+                        let imagePicker = UIImagePickerController()
+                        
+                        // If the device has a camera, take a picture, otherwise,
+                        // just pick from photo library
+                        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                            imagePicker.sourceType = .camera
+                        } else {
+                            imagePicker.sourceType = .photoLibrary
+                        }
+                        
+                        imagePicker.delegate = self
+                        
+                        // Place image picker on the screen
+                        self.present(imagePicker, animated: true, completion: nil)
+                }))
+                
+                present(badImageAlert, animated: true, completion: nil)
+            }
         }
-        
-        imagePicker.delegate = self
-        
-        // Place image picker on the screen
-        present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String: Any]) {
+        
+        // Get picked image from info dictionary
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+
+        // Put that image onto the screen in our image view
+        capturedImage.image = image
+        
+        // Take image picker off the screen -
+        // you must call this dismiss method
+        dismiss(animated: true, completion: nil)
+        print("Dismissal 1")
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func processImage(image: UIImage?) -> [Event]? {
+        
+        
+        return nil
     }
 }
 
