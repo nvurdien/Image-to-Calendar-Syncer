90f9a09748d996fa0940d09a8b568b9f250ab1af
Charles Bucher <charles.abucher@gmail.com>
2018-02-03 16:21:14 -0800

    Basic camera functionality added - doesn't look good yet.

--- Image to Calendar Syncer/ViewController.swift
+++ Image to Calendar Syncer/ViewController.swift
@@ -9,19 +9,33 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let imagePicker = UIImagePickerController()
+        
+        // If the device has a camera, take a picture, otherwise,
+        // just pick from photo library
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        // Place image picker on the screen
+        present(imagePicker, animated: true, completion: nil)
     }
 
 
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
     }
-
-
-
 }
